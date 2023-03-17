1dae20ade4ffa3636518e64f642e112b1a0e5ab1
0Tendo <esaftler@gmail.com>
2023-03-16 20:05:22 -0700

    updated messages for clarity, added comments

--- client.py
+++ client.py
@@ -10,26 +10,16 @@ PORT = 8080
 RECV_BUFFER = 4096 
 
 opcodes = {
-    0 : 'JOIN',
-    1 : 'LEAVE',
-    2 : 'MSG',
-    3 : 'LIST',
-    4 : 'NICK',
-    5 : 'QUIT',
-    6 : 'ERROR',
+    'JOIN': 0,
+    'LEAVE': 1,
+    'MSG': 2,
+    'LIST': 3,
+    'NICK': 4,
+    'QUIT': 5,
+    'ERROR': 6,
 }
 
-# opcodes = {
-#     'JOIN': 0,
-#     'LEAVE': 1,
-#     'MSG': 2,
-#     'LIST': 3,
-#     'NICK': 4,
-#     'QUIT': 5,
-#     'ERROR': 6,
-# }
-
-
+# receive messages from the server
 def receive_message(client):
     while True:
         data = client.recv(RECV_BUFFER)
@@ -39,35 +29,38 @@ def receive_message(client):
         else:
             print(data.decode('utf-8'))
 
+
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.connect((HOST, PORT))
 
+    # start a thread to continuously receive messages from the server
     recv_thread = threading.Thread(target=receive_message, args=(server,))
     recv_thread.daemon = True
     recv_thread.start()
 
+    # continuously check for user input and send to server
     while True:
         try:
             data = input().strip()
             if data:
                 if data.startswith('/list'):
-                    server.send('LIST\n'.encode('utf-8'))
+                    server.send((str(opcodes['LIST']) + '\n').encode('utf-8'))
                 elif data.startswith('/nick'):
-                    server.send(('NICK ' + data[6:]).encode('utf-8'))
+                    server.send((str(opcodes['NICK']) + ' ' + data[6:]).encode('utf-8'))
                 elif data.startswith('/join'):
-                    server.send(('JOIN ' + data[6:]).encode('utf-8'))
+                    server.send((str(opcodes['JOIN']) + ' ' + data[6:]).encode('utf-8'))
                 elif data.startswith('/leave'):
-                    server.send(('LEAVE ' + data[7:]).encode('utf-8'))
+                    server.send((str(opcodes['LEAVE']) + ' ' + data[7:]).encode('utf-8'))
                 elif data.startswith('/quit'):
-                    server.send('QUIT\n'.encode('utf-8'))
+                    server.send((str(opcodes['QUIT']) + '\n').encode('utf-8'))
                     break
                     sys.exit()
                 elif data.startswith('/help'):
                     print('List of commands:\n/list\n/nick\n/join\n/leave\n/quit\n/help')
                 else:
-                    server.send(('MSG ' + data).encode('utf-8'))
+                    server.send((str(opcodes['MSG']) + ' ' + data).encode('utf-8'))
         except:
             continue
 
--- server.py
+++ server.py
@@ -9,11 +9,14 @@ import time
 HOST = '127.0.0.1'
 PORT = 8080
 
-# Set up a list of sockets to keep track of all connected clients
-SOCKET_LIST = []
 RECV_BUFFER = 4096
+# list of clients
+SOCKET_LIST = []
+# dictionary of existing chatrooms
 CHATROOM = {}
+# dictionary of chatrooms for each client
 CLIENT_CHATROOMS = {}
+# dictionary of nicknames for each client
 NICKNAMES = {}
 
 
@@ -53,7 +56,7 @@ def send_message(client, data):
         for sock in SOCKET_LIST:
             if sock != server and sock != client and last_joined_chatroom in CLIENT_CHATROOMS.get(sock, []):
                 # Use NICKNAMES to get the sender's nickname
-                sender = NICKNAMES.get(client, "Anonymous")
+                sender = NICKNAMES.get(client, "Anon")
                 message = '[' + last_joined_chatroom + '] ' + sender + ': ' + data
                 sock.send(message.encode('utf-8'))
     else:
@@ -93,13 +96,13 @@ def error(client, data):
 
 # Define opcodes (commands) for the chatroom
 opcodes = {
-    'JOIN': join_chatroom,
-    'LEAVE': leave_chatroom,
-    'MSG': send_message,
-    'LIST': list_chatrooms,
-    'NICK': change_nick,
-    'QUIT': quit,
-    'ERROR': error,
+    '0': join_chatroom,
+    '1': leave_chatroom,
+    '2': send_message,
+    '3': list_chatrooms,
+    '4': change_nick,
+    '5': quit,
+    '6': error,
 }
 
 # Parse received data to determine opcode and call appropriate function
